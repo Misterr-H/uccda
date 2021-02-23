151e4ef7ecd112eab3753bedf1f4276733912e21
Mister-H <55010619+Misterr-H@users.noreply.github.com>
2021-02-24 05:22:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+
+       'lab-img': "url('img/lab.jpg')",
+
+       
+      })
+    }
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
